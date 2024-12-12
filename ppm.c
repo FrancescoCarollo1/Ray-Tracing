00dd9e8a9afc44c81d96ecf8727a77e393b5f842
Francesco Carollo <carollofrancesco2@gmail.com>
2024-12-12 23:10:58 +0100

    debugging sessio went wrong

--- ppm.c
+++ ppm.c
@@ -1,11 +1,16 @@
 // Francesco Carollo SM3201419
 
+
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include <math.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 
 int scrivi_immagine(const char *filename, unsigned char *data, int width, int height)
 {
@@ -25,14 +30,16 @@ int scrivi_immagine(const char *filename, unsigned char *data, int width, int he
         return 1;
     }
 
-    struct stat statbuf;
-    if (stat(filename, &statbuf) < 0)
-    {
-        perror("stat error");
-        return 1;
-    }
 
-    void *addr = mmap(NULL, statbuf.st_size, PROT_WRITE, MAP_SHARED, fd, 0);
+    int w = (int)((ceil(log10(width)))*sizeof(char));
+    int h = (int)((ceil(log10(height)))*sizeof(char)); 
+    int preambolo_size = 3 + w + 1+ h + 5 + 1;    
+    int data_size = width * height * 3;
+    printf("preambolo_size: %d\n", preambolo_size);
+    printf("data_size: %d\n", data_size);
+
+
+    void *addr = mmap(NULL,preambolo_size + data_size, PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == MAP_FAILED)
     {
         perror("Error mapping the file");
@@ -40,18 +47,16 @@ int scrivi_immagine(const char *filename, unsigned char *data, int width, int he
     }
 
 
-    int w = (int)((ceil(log10(width))+1)*sizeof(char));
-    int h = (int)((ceil(log10(height))+1)*sizeof(char)); 
-    int preambolo_size = 3 + w + 1+ h + 5 + 1;
+
     char *preambolo = (char*) malloc((preambolo_size)*sizeof(char));
     sprintf(preambolo, "P6\n%d %d\n255\n", width, height);
 
 
     memcpy(addr, preambolo, preambolo_size);
 
-    memcpy((char*)addr + preambolo_size, data, width * height * 3);
+    memcpy((char*)addr + preambolo_size, data, data_size);
     free(preambolo);
-    munmap(addr, statbuf.st_size);
+    munmap(addr, preambolo_size + data_size);
     fclose(file);
     
     return 0;
