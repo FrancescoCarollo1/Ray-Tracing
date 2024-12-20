555eae314f584d883fc77be6f2be8f3d5e764d73
Francesco Carollo <carollofrancesco2@gmail.com>
2024-12-20 11:43:16 +0100

    added some paralelization tests

--- src/render.h
+++ src/render.h
@@ -7,3 +7,5 @@
 //Define the function prototypes
 void omp_render_scene (Scene *scene, Color *pixel_out, int width, int height);
 void render_scene (Scene *scene, Color *pixel_out, int width, int height);
+Color colore_raggio(Vec3 ray, Scene *scene);
+Color omp_colore_raggio(Vec3 ray, Scene *scene);
