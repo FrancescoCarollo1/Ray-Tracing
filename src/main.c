90bcabfa760aa676935b061bf907458c7d77c43c
Francesco Carollo <carollofrancesco2@gmail.com>
2025-01-01 21:26:11 +0100

    creazione dei file test che contegono le funzioni per misurare la velocità delle funzioni di rendering

--- src/main.c
+++ src/main.c
@@ -11,35 +11,7 @@
 
 
 
-double test_render_scene(void (* render_scene)(Scene *, Color *, int, int))
-{
-    int width = 1920;
-    int height = 1080;
-    Color *data = malloc(width * height * 3);
-    memset(data, 0, width * height * 3);
 
-    Scene *scene = malloc(sizeof(Scene));
-    read_scene("prova.txt", scene);
-
-    double start = omp_get_wtime();
-    render_scene(scene, data, width, height);
-    double end = omp_get_wtime();
-    double ms = (end - start) * 1000;
-    return ms;
-    printf("Time: %f ms\n", ms);
-    return 0;
-}
-
-
-double test_colore_raggio(Color (*colore_raggio)(Vec3 ray, Scene *scene))
-{
-    double start = omp_get_wtime();
-    double end = omp_get_wtime();
-    double ms = (end - start) * 1000;
-    return ms;
-    printf("Time: %f ms\n", ms);
-    return 0;
-}
 
 
 
@@ -52,9 +24,9 @@ int main()
     Color *data = malloc(width * height * sizeof(Color));
     memset(data, 0, width * height * sizeof(Color));
     Scene *scene = create_empty_scene();
-    read_scene("prove_txt/prova11.txt", scene);
+    read_scene("prove_txt/prova8.txt", scene);
     render_scene(scene, data, width, height);
-    scrivi_immagine("renders/prova11.ppm", data, width, height);
+    scrivi_immagine("renders/prova8.ppm", data, width, height);
 
     free(data);
     delete_scene(scene);
---
+++ src/test.h
@@ -0,0 +1,7 @@
+
+#include "scene.h"
+#include "vec3.h"
+
+
+double test_render_scene(void (* render_scene)(Scene *, Color *, int, int))
+double test_colore_raggio(Color (*colore_raggio)(Vec3 ray, Scene *scene))
